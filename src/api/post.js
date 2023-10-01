11a66411856fad256c5a980c5b7e89c3c8142121
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-10-01 22:24:56 +0400

    feat(post): created createNewTask function for a post request

--- src/api/post.js
+++ src/api/post.js
@@ -25,3 +25,26 @@ export const createNewProject = async (projectName) => {
   }
 };
 
+export const createNewTask = async (taskContent, projectId) => {
+    try {
+      const response = await fetch('https://api.todoist.com/rest/v2/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          content: taskContent,
+          project_id: projectId,
+        }),
+      });
+      if (response.ok) {
+        const newTask = await response.json();
+        return newTask;
+      }
+      throw new Error('Failed to create task');
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
