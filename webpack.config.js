15b074035e4ce0911242cbd55c0da5bd1b563474
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-10-01 22:12:10 +0400

    fix(webpack): changed require.resolve to require

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require.resolve('path')
+const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
