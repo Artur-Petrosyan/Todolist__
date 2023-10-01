324e6a707ae515dac182397bf3b54b91bc3d9f98
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-10-01 22:38:18 +0400

    feat(complited): created showTasks function

--- complited.js
+++ complited.js
@@ -14,3 +14,24 @@ const loader = document.querySelector('.loader-container');
 const taskContainer = document.querySelector('.tasks');
 const logOutButton = document.querySelector('.log-out__button');
 const authorized = userAuthorized();
+
+const showTasks = async () => {
+    if (authorized) {
+      showLoader(loader);
+      const allTasks = await getAllTasks();
+      const completedTasks = getFilteredTasks(allTasks, 'completed');
+  
+      let taskHTML = '';
+  
+      completedTasks.forEach((task) => {
+        taskHTML += createTaskHTML(task);
+      });
+  
+      taskContainer.innerHTML = taskHTML;
+  
+      addDeleteButtonEventListeners(taskContainer);
+      checkBoxesList(taskContainer);
+    } else {
+      logOutUser();
+    }
+  };
