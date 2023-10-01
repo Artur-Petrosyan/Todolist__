25ce2a33a64980eec69d2520c50c5a8519fec13e
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-10-01 18:14:48 +0400

    feat(webpack): added a plugins field

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
 const path = require.resolve('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: {
-         index : './src/index.js',
-         login: './src/login.js',
-        complited: "./src/complited.js",
-        active: "./src/active.js",
-        all: "./src/all.js",
-        api_delete: "./src/api/delete.js",
-        api_get: "./src/api/get.js",
-        api_patch: "./src/api/patch.js",
-        api_post: "./src/api/post.js",
-        api_token : "./src/api/token.js",
-        utils: './src/utils/utils.js',
-    },
+  entry: {
+    index: './src/index.js',
+    login: './src/login.js',
+    complited: "./src/complited.js",
+    active: "./src/active.js",
+    all: "./src/all.js",
+    api_delete: "./src/api/delete.js",
+    api_get: "./src/api/get.js",
+    api_patch: "./src/api/patch.js",
+    api_post: "./src/api/post.js",
+    api_token: "./src/api/token.js",
+    utils: './src/utils/utils.js',
+  },
 
-    output: {
-      filename : '[name].[contenthash].js',
-      path : path.resolve(__dirname,'dist')
-    }
-
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins : {
+    
+  }
 
 }
