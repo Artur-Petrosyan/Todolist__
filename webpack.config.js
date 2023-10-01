0985eb0ba905ffe91b165bf231b46ff263deadeb
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-10-01 18:04:39 +0400

    feat(webpack): added a new entry point in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,17 @@ const path = require.resolve('path')
 
 module.exports = {
     entry: {
-         index : './src/index.js'
+         index : './src/index.js',
+         login: './src/login.js',
+        complited: "./src/complited.js",
+        active: "./src/active.js",
+        all: "./src/all.js",
+        api_delete: "./src/api/delete.js",
+        api_get: "./src/api/get.js",
+        api_patch: "./src/api/patch.js",
+        api_post: "./src/api/post.js",
+        api_token : "./src/api/token.js",
+        utils: './src/utils/utils.js',
     },
 
     output: {
